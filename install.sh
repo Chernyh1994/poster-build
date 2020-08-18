9870e643fc5977f8a966083078fc9dbdd023cf9c
Nickolay <chernuh.1994@gmail.com>
2020-08-18 19:14:55 +0300

    comment updated startup scrips

--- install.sh
+++ install.sh
@@ -3,10 +3,32 @@
 GIT_NAME="$(git config user.name)"
 GIT_EMAIL="$(git config user.email)"
 
-if [[ -z $GIT_NAME || -z $GIT_EMAIL ]]
- then
-  echo "Please configure git user name and email" && exit -1
+# Function check if the line is empty by the user.
+function checkDefault() {
+  while read -r -p "$1" DATA; do
+    DATA=${DATA,,}
+    if [[ "$DATA" =~ ^()$ ]]; then
+      continue
+    else
+      echo "$DATA"
+      break
+    fi
+  done
+}
+
+if [[ -z $GIT_NAME || -z $GIT_EMAIL ]]; then
+
+  NAME=$(checkDefault "Please enter your git name $(tput setaf 1)(required)$(tput sgr0): ")
+  git config user.name "$NAME"
+  EMAIL=$(checkDefault "Please enter your git email $(tput setaf 1)(required)$(tput sgr0): ")
+  git config user.email "$EMAIL"
+
 fi
 
 git submodule init
 git submodule update
+
+composer install  --working-dir=$PWD/poster-laravel-api-server --ignore-platform-reqs
+yarn --cwd $PWD/poster-client-react
+
+docker-compose up --build -d proxy
